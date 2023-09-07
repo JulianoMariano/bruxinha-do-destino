30c4b3cd27b26e03686bce65b5625f2454df1a27
Juliano Mariano <julianoaugusto.xp6@gmail.com>
2023-09-07 17:02:57 -0300

    edit queryselector

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
+const elementResponse = document.querySelector("#responses")
+const responses = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
 
 function askQuestion () {
   
-  const responses = [
-    "Certeza!",
-    "Não tenho tanta certeza.",
-    "É decididamente assim.",
-    "Não conte com isso.",
-    "Sem dúvidas!",
-    "Pergunte novamente mais tarde.",
-    "Sim, definitivamente!",
-    "Minha resposta é não.",
-    "Você pode contar com isso.",
-    "Melhor não te dizer agora.",
-    "A meu ver, sim.",
-    "Minhas fontes dizem não.",
-    "Provavelmente.",
-    "Não é possível prever agora.",
-    "Perspectiva boa.",
-    "As perspectivas não são tão boas.",
-    "Sim.",
-    "Concentre-se e pergunte novamente.",
-    "Sinais apontam que sim.",
-  ]
-  const totalResponses = resposes.length
+  const totalResponses = responses.length
   const aleatoryNumber = Math.floor(Math.random() * totalResponses)
-  
+
+  elementResponse.innerHTML = responses[aleatoryNumber]
+
 }
